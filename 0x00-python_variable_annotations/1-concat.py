2c047625bab9f5eb0ac3595293c4a775f697a103
Esther Kubania <122841197+dev-EstherKubania@users.noreply.github.com>
2024-03-14 23:13:59 +0300

    Create 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module to demonstrate type annotations in Python
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string.
+    """
+    return str1 + str2
